35c031ff9150effa9e2c4a78d0f025242479a6bc
Patrick Pietens <hello@patrickpietens.com>
2016-11-29 00:05:09 +0100

    Updated readme

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 import assert from 'assert';
 
-DOMTokenList.prototype.cond = function(...args) {
+DOMTokenList.prototype.bool = function(...args) {
     const value = args.pop();
     const tokens = args;
 
-    assert(args.length >= 1, 'The cond method accepts 2 or more arguments. Only ${args.length} argument are given.');
-    assert (typeof value === 'boolean', 'The last arguments should be a boolean');
+    assert(args.length >= 1, `The cond method accepts 2 or more arguments. Only ${args.length} argument are given.`);
+    assert(typeof value === 'boolean', 'The last argument of the cond method should be a boolean');
 
     if (value) {
         this.add(tokens);
